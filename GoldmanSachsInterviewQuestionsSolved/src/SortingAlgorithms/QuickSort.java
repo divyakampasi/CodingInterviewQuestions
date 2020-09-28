262e0a533d1e7b5c47f93c816849148c21b6c499
Divya Kampasi <divyakampasi03@gmail.com>
2020-09-28 19:53:36 +0530

    Implemented Quick Sort

---
+++ GoldmanSachsInterviewQuestionsSolved/src/SortingAlgorithms/QuickSort.java
@@ -0,0 +1,51 @@
+package SortingAlgorithms;
+
+public class QuickSort {
+	
+	int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = (low-1);
+		for(int j=low;j<high;j++) {
+			 if (arr[j] < pivot) 
+	            { 
+	                i++; 
+	                int temp = arr[i]; 
+	                arr[i] = arr[j]; 
+	                arr[j] = temp; 
+	            } 
+		}
+		int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+	}
+	
+	void sort(int arr[], int low, int high) {
+		if(low<high) {
+			int p = partition(arr, low, high);
+			sort(arr,low,p-1);
+			sort(arr,p+1,high);
+		}
+	}
+	
+	static void printArray(int arr[]) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {2,3,22,54,10,7,8,9,1,5};
+		int n = arr.length;
+		
+		QuickSort ob = new QuickSort();
+		ob.sort(arr,0,n-1);
+		
+		System.out.println("Sorted Array :");
+		printArray(arr);
+
+	}
+
+}
