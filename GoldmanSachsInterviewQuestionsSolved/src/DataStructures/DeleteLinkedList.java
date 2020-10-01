f9a583d8c6504e1d1864ec319f508ee0c310aabc
Divya Kampasi <divyakampasi03@gmail.com>
2020-10-01 12:32:53 +0530

    Delete a given linked list

---
+++ GoldmanSachsInterviewQuestionsSolved/src/DataStructures/DeleteLinkedList.java
@@ -0,0 +1,54 @@
+package DataStructures;
+
+public class DeleteLinkedList {
+	
+	Node head;
+	static class Node{
+		int data;
+		Node next;
+		
+		Node(int d){
+			data = d;
+			next = null;
+		}
+	}
+	
+	void deleteList() {
+		head = null;
+	}
+	
+	public void push(int new_data) {
+		Node new_node = new Node(new_data);
+		if(head == null) {
+			head = new_node;
+			return;
+		}
+		new_node.next = head;
+		head = new_node;
+	}
+	
+	public void printList() {
+		Node n = head;
+		if(n==null) {
+			System.out.println("Linked List Empty");
+		}
+		while(n!=null) {
+			System.out.print(n.data+" ");
+			n=n.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		DeleteLinkedList l = new DeleteLinkedList();
+		l.push(1);
+		l.push(2);
+		l.push(3);
+		
+		l.printList();
+		l.deleteList();
+		l.printList();
+		
+	}
+
+}
