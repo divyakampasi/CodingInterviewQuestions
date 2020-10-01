8491f5ffb70f1a44999b5b09663ca7b1bf9944b7
Divya Kampasi <divyakampasi03@gmail.com>
2020-10-01 16:04:34 +0530

    time complexity improved

--- GoldmanSachsInterviewQuestionsSolved/src/Problems/EfficientTrappingRainWaterSolution.java
+++ GoldmanSachsInterviewQuestionsSolved/src/Problems/EfficientTrappingRainWaterSolution.java
@@ -27,7 +27,7 @@ public class EfficientTrappingRainWaterSolution {
 	}
 
 	public static void main(String[] args) {
-		int arr[] = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }; 
+		int arr[] = new int[] { 3,0,2,0,4,0,6}; 
 		System.out.println("Maximum water that can be accumulated is " + findWater(arr, arr.length));
 	}
 }
